f5428a0574da3b5e4850d4ae8c2298e6388873b7
rann03 <psychorania03@gmail.com>
2025-05-02 21:11:43 +0100

    Track ingested & preprocessed movie plots data with DVC

--- move_plot_feast/feature_repo/feature_views.py
+++ move_plot_feast/feature_repo/feature_views.py
@@ -2,6 +2,7 @@
 from feast import Entity, FeatureView, Field, FileSource, ValueType
 from feast.types import Float32, String
 import os
+from datetime import datetime
 
 # Define entity
 movie = Entity(name="movie_id", value_type=ValueType.STRING, description="Unique movie id")
@@ -11,7 +12,7 @@ data_dir = os.path.join(os.path.dirname(__file__), "data")
 movie_features_source = FileSource(
     path=os.path.join(data_dir, "movie_features.parquet"),
     event_timestamp_column="event_timestamp",
-    created_timestamp_column="event_timestamp",
+    # Remove created_timestamp_column or use a different column
 )
 
 # Define feature view
@@ -20,8 +21,8 @@ movie_features_view = FeatureView(
     entities=[movie],
     ttl=None,
     schema=[
-        Field(name="tfidf_1", dtype=Float32),  # Changed to Float32
-        Field(name="tfidf_2", dtype=Float32),  # Changed to Float32
+        Field(name="tfidf_1", dtype=Float32),
+        Field(name="tfidf_2", dtype=Float32),
     ],
     source=movie_features_source,
     online=True,
